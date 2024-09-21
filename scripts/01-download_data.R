5091aba2a7cb961bcc0017d2b5f14d0f33b7d2e9
stevenli-uoft <stevency.li@mail.utoronto.ca>
2024-09-21 16:08:22 -0400

    Added script to pull bikeshare data

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,26 +1,53 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
-# License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from Open Data Toronto
+# Author: Steven Li
+# Date: 20 September 2024
+# Contact: stevency.li@mail.utoronto.ca
 
-
-#### Workspace setup ####
+# Load required libraries
 library(opendatatoronto)
 library(tidyverse)
-# [...UPDATE THIS...]
+library(fs)  # For file system handling
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
+# get package details
+package <- show_package("7e876c24-177c-4605-9cef-e50dd74c617f")
 
+# get all resources for this package
+resources <- list_package_resources("7e876c24-177c-4605-9cef-e50dd74c617f")
 
+# identify datastore resources (ZIP files)
+datastore_resources <- filter(resources, format == 'ZIP')
 
-#### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+# Create directory to store raw data CSV files (clear if it exists first)
+raw_data_dir <- "data/raw_data/"
 
-         
+# Check if the directory exists, if it does, delete it
+if (dir_exists(raw_data_dir)) {
+  dir_delete(raw_data_dir)  # Delete the folder and its contents
+}
+
+# Recreate the directory after deletion
+dir_create(raw_data_dir)
+
+#### Download and process each resource ####
+for(i in 1:nrow(datastore_resources)) {
+  # Get the resource ID and name
+  resource_id <- datastore_resources$id[i]
+  resource_name <- datastore_resources$name[i]
+  
+  # Download the resource using its ID
+  resource_data <- get_resource(resource_id)
+  
+  # Iterate over the list of tibbles in the resource data
+  for (name in names(resource_data)) {
+    
+    # Check if the object is a tibble (skip non-data objects)
+    if (is_tibble(resource_data[[name]]) && nrow(resource_data[[name]]) > 1) {
+      
+      # Save the individual CSV file to the raw_data directory
+      file_path <- file.path(raw_data_dir, name)
+      write_csv(resource_data[[name]], file_path)
+    }
+  }
+}
